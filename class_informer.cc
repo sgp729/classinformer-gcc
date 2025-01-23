97718f8eb96ae9da108063a08651b6e218c8a6d5
Georgiy Smykov <sgp@alram.in>
2025-01-23 15:41:37 +0300

    add plugin run() routine

--- class_informer.cc
+++ class_informer.cc
@@ -1,10 +1,37 @@
 // Copyright (C) 2025 Georgiy Smykov
 #include "class_informer.h"
+#include "rtti.h"
+#include "display.h"
+#include "storage.h"
 #include <kernwin.hpp>
 
 namespace class_informer {
 
 bool plugin::run(size_t arg) {
+
+        rtti::vtables vts;
+        if (storage::has_persistent_data()) {
+
+                int ans = ask_yn(ASKBTN_YES, "There is an existing VTable DB; Open it?");
+                if (ans == ASKBTN_CANCEL) {
+                        return true;
+                }
+                else if (ans == ASKBTN_YES) {
+                        vts = storage::get_persistent_data();
+                }
+                else {
+                        goto nodata;
+                }
+        }
+        else {
+nodata:
+                vts = rtti::find_vtables_in_segment(".data.rel.ro");
+                storage::store_persistent_data(vts);
+        }
+
+        chooser_t* ch = new display::chooser(vts);
+        ch->choose();
+
         return true;
 }
 
